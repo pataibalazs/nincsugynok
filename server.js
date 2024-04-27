06ec9431328477fcc07f095ca11126934e842839
pataibalazs <79509500+pataibalazs@users.noreply.github.com>
2024-04-27 14:34:23 +0200

    test2

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', function (req, res) {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
